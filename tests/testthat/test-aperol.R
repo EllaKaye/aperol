e7ef2ef313e4b8e6f6c87541711d0a33f122b9ad
EllaKaye <ella.kaye@gmail.com>
2024-07-18 08:18:20 +0100

    Use withr::local_seed (closes #3)

--- tests/testthat/test-aperol.R
+++ tests/testthat/test-aperol.R
@@ -1,11 +1,11 @@
 test_that("tipsy works", {
-  set.seed(1)
+  withr::local_seed(1)
   expect_equal(tipsy(), "tenderly are You impeccable!")
   expect_equal(tipsy(2, "You are ${creating} a ${adverb} ${adjective} ${rpackage}"), "You deliberately initiating a code first-rate are")
 })
 
 test_that("drunk works", {
-  set.seed(1)
+  withr::local_seed(1)
   expect_equal(drunk(), "You are tenderly tenderly impeccable!")
   expect_equal(drunk(2, 2:3), "You are first-rate! deliberately You first-rate! first-rate!")
   expect_equal(drunk(2, 2, "You are ${creating} a ${adverb} ${adjective} ${rpackage}"), "are a project You perfectly composing are perfectly sensational")
