bf1cc71c9f5b62f32846a975af60c8aa361a243e
EllaKaye <ella.kaye@gmail.com>
2024-08-02 17:30:20 +0100

    apply styler

--- R/spritz.R
+++ R/spritz.R
@@ -1,6 +1,6 @@
 # create a new environment and an object called spritz count
 the <- new.env(parent = emptyenv())
-the$spritz_count <- data.frame(count = numeric(),time = numeric(),func = character())
+the$spritz_count <- data.frame(count = numeric(), time = numeric(), func = character())
 
 
 #' Spritz
@@ -27,56 +27,51 @@ the$spritz_count <- data.frame(count = numeric(),time = numeric(),func = charact
 #' spritz(runs = 4, minutes = 15)
 #'
 #' ## custom function
-#' sleep_add <- function(){
+#' sleep_add <- function() {
 #'   Sys.sleep(2)
-#'   out <- 1+1
+#'   out <- 1 + 1
 #'   return(out)
 #' }
 #'
 #' spritz(func = "sleep_add()")
-#'
-#'
 #' }
 #'
-#'
 spritz <- function(runs = 3, minutes = 10, template = "You are ${adverb} ${adjective}!",
-                   func =  "devtools::check()"){
-
-
+                   func = "devtools::check()") {
   # fitler by time
-  time_frame <- (as.numeric(Sys.time())-(minutes*60))
+  time_frame <- (as.numeric(Sys.time()) - (minutes * 60))
 
-  spritz_count_filtered  <- the$spritz_count[the$spritz_count$time >= time_frame & the$spritz_count$func == func, ]
+  spritz_count_filtered <- the$spritz_count[the$spritz_count$time >= time_frame & the$spritz_count$func == func, ]
   # check number of runs
 
-   runs_in_time_frame <- sum(spritz_count_filtered$count)
+  runs_in_time_frame <- sum(spritz_count_filtered$count)
 
-   # normal praise
-  if(runs_in_time_frame == runs){
+  # normal praise
+  if (runs_in_time_frame == runs) {
     praise <- praise::praise(template = template)
     message(praise)
   }
 
-   diff_runs  <- runs_in_time_frame - runs
+  diff_runs <- runs_in_time_frame - runs
 
-   # tipsy praise
-   if(diff_runs < 3 & diff_runs > 0 ){
-     message(tipsy(template = template))
-   }
+  # tipsy praise
+  if (diff_runs < 3 & diff_runs > 0) {
+    message(tipsy(template = template))
+  }
 
-   # drunk praise
-   praise_length <- length(strsplit(template, " ")[[1]])
-   rep_n <- min(diff_runs - 2, praise_length)
+  # drunk praise
+  praise_length <- length(strsplit(template, " ")[[1]])
+  rep_n <- min(diff_runs - 2, praise_length)
 
-   if (diff_runs >= 3) {
-     message(drunk(repeat_words = rep_n, repeat_times = 2:(rep_n + 1), template = template))
-   }
+  if (diff_runs >= 3) {
+    message(drunk(repeat_words = rep_n, repeat_times = 2:(rep_n + 1), template = template))
+  }
 
-   # increment the spritz count
-   increment_spritz_count(func)
+  # increment the spritz count
+  increment_spritz_count(func)
 
-   # run the function
-   out  <- eval(parse(text = func))
+  # run the function
+  out <- eval(parse(text = func))
 
   return(out)
 }
@@ -92,12 +87,11 @@ spritz <- function(runs = 3, minutes = 10, template = "You are ${adverb} ${adjec
 #' @export
 #'
 #' @examples
-#'  increment_spritz_count(func = "1+1")
+#' increment_spritz_count(func = "1+1")
 #'
-increment_spritz_count <- function(func){
-  count_item <- data.frame(count = 1,time = as.numeric(Sys.time()),func = func)
+increment_spritz_count <- function(func) {
+  count_item <- data.frame(count = 1, time = as.numeric(Sys.time()), func = func)
   old <- the$spritz_count
-  the$spritz_count <- rbind(old,count_item)
+  the$spritz_count <- rbind(old, count_item)
   invisible(old)
 }
-
